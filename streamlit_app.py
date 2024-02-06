505e387a57272d2b4101aa4e46c0947c58fb1882
bhumikabhambri <158564054+bhumikabhambri@users.noreply.github.com>
2024-02-06 11:09:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,12 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit" + fruit_choice)
 
 #take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+try:
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+except ValueError as e:
+    print(f"Error decoding JSON: {e}")
+    # Handle the error as needed
+
 #output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
