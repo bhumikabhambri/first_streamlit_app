ae6a6253837bfa9cf4af37373084fdcc08006c8d
bhumikabhambri <158564054+bhumikabhambri@users.noreply.github.com>
2024-02-06 13:27:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,6 +70,3 @@ if streamlit.button('Add a Fruit to the List'):
   streamlit.text(back_from_function)
   my_cnx.close()  # Close the connection
 #streamlit.write('Thanks for adding ', add_my_fruit)
-
-#This will not work correctly, but just go with it for now
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
